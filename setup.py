c5512b71ae2adf19ab34f98abcfbc2a3ff31f035
IaroslavR <iarruss@ya.ru>
2017-10-09 15:57:58 +0300

    [fix] setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import setuptools
 import os
 
-import pypandoc
-
-with open('README.rst', 'w') as f:
-    f.write(pypandoc.convert('README.md', 'rst'))
-
-os.environ.update(
-    SKIP_GENERATE_AUTHORS='1',
-    # SKIP_WRITE_GIT_CHANGELOG='1'
-)
+if "PY_DEV" in os.environ:
+    import pypandoc
+    with open('README.rst', 'w') as f:
+        f.write(pypandoc.convert('README.md', 'rst'))
+else:
+    os.environ.update(SKIP_WRITE_GIT_CHANGELOG='1')
+os.environ.update(SKIP_GENERATE_AUTHORS='1')
 setuptools.setup(
     setup_requires=['pbr', ],
     pbr=True
